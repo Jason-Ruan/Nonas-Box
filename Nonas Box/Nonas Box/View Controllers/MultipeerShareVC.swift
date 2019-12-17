85339fcb71a3d9facef58e1926dc6aa4238a4c63
Jason Ruan <jasonruan@pursuit.org>
2019-12-17 11:25:36 -0500

    Adds vc to main.storyboard for multipeer connectivity

---
+++ Nonas Box/Nonas Box/View Controllers/MultipeerShareVC.swift
@@ -0,0 +1,30 @@
+//
+//  MultipeerShareVC.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 12/16/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class MultipeerShareVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

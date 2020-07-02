787bd3791e7e92678d640c542bbc2148ee0c8777
Jason Ruan <jasonruan@pursuit.org>
2020-07-01 23:18:32 -0400

    Adds SearchRecipesOnlineVC and changes background color

--- Nonas Box/Nonas Box/View Controllers/SearchRecipesOnlineVC.swift
+++ Nonas Box/Nonas Box/View Controllers/SearchRecipesOnlineVC.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2020 Jason Ruan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SearchRecipesOnlineVC: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    }
+}

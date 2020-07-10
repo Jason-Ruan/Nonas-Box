da5276261e52a484b43384541c93cb4c0a5897f4
Jason Ruan <jasonruan@pursuit.org>
2020-07-09 22:48:59 -0400

    Adds Spoonacular API Client

--- Nonas Box/Nonas Box/Networking/API Clients/SpoonacularClient.swift
+++ Nonas Box/Nonas Box/Networking/API Clients/SpoonacularClient.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class SpoonacularAPIClient {
+    
+    func getRecipes(query: String, completionHandler: @escaping (Result<[Recipe], AppError>) -> () ) {
+        let formattedQuery = query.replacingOccurrences(of: " ", with: "%20")
+        let urlString = "https://api.spoonacular.com/recipes/search?query=\(formattedQuery)&apiKey=\(Secrets.spoonacular_api_key)"
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(AppError.badURL))
+            return
+        }
+        
+        NetworkHelper.manager.performDataTask(withUrl: url) { (result) in
+            switch result {
+                case .success(let data):
+                    do {
+                        let recipeResults = try JSONDecoder().decode(SpoonacularResults.self, from: data)
+                        guard let recipes = recipeResults.results else {
+                            completionHandler(.failure(AppError.invalidJSONResponse))
+                            return
+                        }
+                        completionHandler(.success(recipes))
+                    } catch {
+                        completionHandler(.failure(AppError.couldNotParseJSON(rawError: error)))
+                }
+                
+                case .failure:
+                    completionHandler(.failure(AppError.noDataReceived))
+            }
+        }
+    }
+    
+    private init() {}
+    static let manager = SpoonacularAPIClient()
+    
+}

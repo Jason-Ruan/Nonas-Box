a790ee789b35c1c602cbe94e60a69b9b66423b58
Jason Ruan <jasonruan@pursuit.org>
2020-07-11 22:23:53 -0400

    Edits SearchRecipesOnlineVC

--- Nonas Box/Nonas Box/View Controllers/SearchRecipesOnlineVC.swift
+++ Nonas Box/Nonas Box/View Controllers/SearchRecipesOnlineVC.swift
@@ -9,7 +9,207 @@
 import UIKit
 
 class SearchRecipesOnlineVC: UIViewController {
-    override func viewDidLoad() {
-        view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+    //MARK: - UI Objects
+    
+    lazy var screenTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Azure Overall - Recipe Search"
+        label.font = .boldSystemFont(ofSize: 25)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.delegate = self
+        sb.placeholder = "Search for recipes here"
+        return sb
+    }()
+    
+    lazy var recipeCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.frame.width / 2.5, height: view.frame.width / 2.5)
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = 5
+        
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .clear
+        cv.register(RecipeCollectionViewCell.self, forCellWithReuseIdentifier: "recipeCell")
+        cv.dataSource = self
+        cv.delegate = self
+        cv.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        return cv
+    }()
+    
+    lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+        spinner.color = .lightGray
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+    
+    lazy var blurEffectView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        return blurEffectView
+    }()
+    
+    
+    //MARK: - Private Properties
+    
+    private var recipes: [Recipe] = [] {
+        didSet {
+            recipeCollectionView.reloadData()
+        }
     }
+    
+    //MARK: - LifeCycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUpViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    //MARK: - Private Functions
+    
+    private func setUpViews() {
+        
+        view.addSubview(screenTitleLabel)
+        screenTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            screenTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            screenTitleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            screenTitleLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            screenTitleLabel.heightAnchor.constraint(equalToConstant: view.frame.height / 10)
+        ])
+        
+        view.addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: screenTitleLabel.bottomAnchor),
+            searchBar.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            searchBar.widthAnchor.constraint(equalToConstant: view.frame.width),
+            searchBar.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        view.addSubview(recipeCollectionView)
+        recipeCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            recipeCollectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            recipeCollectionView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            recipeCollectionView.widthAnchor.constraint(equalToConstant: view.frame.width),
+            recipeCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+    
+    private func showLoadingAnimation() {
+        view.addSubview(blurEffectView)
+        blurEffectView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            blurEffectView.topAnchor.constraint(equalTo: recipeCollectionView.topAnchor),
+            blurEffectView.centerXAnchor.constraint(equalTo: recipeCollectionView.centerXAnchor),
+            blurEffectView.widthAnchor.constraint(equalToConstant: recipeCollectionView.frame.width),
+            blurEffectView.bottomAnchor.constraint(equalTo: recipeCollectionView.bottomAnchor)
+        ])
+        
+        view.addSubview(spinner)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            spinner.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+        
+        spinner.startAnimating()
+        
+    }
+    
+    private func removeLoadingAnimation() {
+        self.spinner.removeFromSuperview()
+        self.blurEffectView.removeFromSuperview()
+    }
+    
+    private func showNoResultsAlert() {
+        let alert = UIAlertController(title: "Oops", message: "Sorry, that search had no results in our database.", preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
+        let waitTime = DispatchTime.now() + 1
+        DispatchQueue.main.asyncAfter(deadline: waitTime, execute: {
+            alert.dismiss(animated: true, completion: nil)
+        })
+    }
+    
 }
+
+//MARK: - SearchBar Methods
+
+extension SearchRecipesOnlineVC: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.showsCancelButton = true
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        guard let query = searchBar.text else { return }
+        //        showLoadingAnimation()
+        
+        SpoonacularAPIClient.manager.getRecipes(query: query) { (result) in
+            
+            //            self.removeLoadingAnimation()
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let recipes):
+                        guard !recipes.isEmpty else {
+                            self.showNoResultsAlert()
+                            return
+                        }
+                        self.recipes = recipes
+                    case .failure(let error):
+                        print(error)
+                }
+            }
+        }
+        
+        if recipes.count > 0 {
+            recipeCollectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+        }
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.resignFirstResponder()
+    }
+    
+}
+
+//MARK: - CollectionView Methods
+
+extension SearchRecipesOnlineVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        recipes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath) as? RecipeCollectionViewCell else { return UICollectionViewCell()}
+        cell.recipe = recipes[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let detailVC = DetailVC()
+//        detailVC.recipe = recipes[indexPath.row]
+//        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+}
+
+

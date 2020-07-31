b84f27428ad14163a1adce81075b5fed383a4674
Jason Ruan <jasonruan@pursuit.org>
2020-07-30 23:33:28 -0400

    Adds UPC_Item model and UPC-ItemDB_Client

---
+++ Nonas Box/Nonas Box/Models/UPC_Item.swift
@@ -0,0 +1,22 @@
+//
+//  UPC_Item.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 7/27/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import Foundation
+
+struct UPC_Item_Results: Codable {
+    let items: [UPC_Item]
+}
+
+struct UPC_Item: Codable {
+    let ean: String?
+    let upc: String?
+    let title: String?
+    let description: String?
+    let brand: String?
+    let images: [URL]?
+}
---
+++ Nonas Box/Nonas Box/Networking/API Clients/UPC_ItemDB_Client.swift
@@ -0,0 +1,9 @@
+//
+//  UPC_ItemDB_Client.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 7/27/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import Foundation

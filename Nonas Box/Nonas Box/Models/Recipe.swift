b709fd716883c403e25937f4b25130e37b3964bd
Jason Ruan <jasonruan@pursuit.org>
2020-08-14 23:50:19 -0400

    Adds RecipeDetails struct and relevant models to Recipe file

--- Nonas Box/Nonas Box/Models/Recipe.swift
+++ Nonas Box/Nonas Box/Models/Recipe.swift
@@ -25,14 +25,53 @@ struct Recipe: Codable {
 
 }
 
-extension Recipe: Hashable {
-    
-    static func == (lhs: Recipe, rhs: Recipe) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
+struct RecipeDetails: Codable {
+    let summary: String?
+    let extendedIngredients: [Ingredient]
+    let analyzedInstructions: [StepByStepInstructions]?
 }
+
+struct StepByStepInstructions: Codable {
+    let steps: [Step]?
+}
+
+struct Step: Codable {
+    let number: Int?
+    let step: String?
+}
+
+
+struct Ingredient: Codable {
+    let id: Int?
+    let name: String?
+    let originalString: String?
+    let image: String?
+    let measures: IngredientMeasurements
+    
+    var imageURL: URL? {
+        guard let image = image else { return nil }
+        return URL(string: "https://spoonacular.com/cdn/ingredients_100x100/\(image)")
+    }
+}
+
+struct IngredientMeasurements: Codable {
+    let us: Measurement
+    let metric: Measurement
+}
+
+struct Measurement: Codable {
+    let amount: Double?
+    let unitShort: String?
+    let unitLong: String?
+    
+    var shortHandMeasurement: String? {
+        guard let amount = amount, let unitShort = unitShort else { return nil }
+        return "\(amount) \(unitShort)"
+    }
+    
+    var fullMeasurement: String? {
+        guard let amount = amount, let unitLong = unitLong else { return nil }
+        return "\(amount) \(unitLong)"
+    }
+}
+

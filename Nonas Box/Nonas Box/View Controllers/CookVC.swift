ea6ab80e989ca2c08fdfbf69496ab95bfa2c70b3
Jason Ruan <jasonruan@pursuit.org>
2020-08-31 22:38:28 -0400

    Adds CookVC file and vc to main.storyboard

---
+++ Nonas Box/Nonas Box/View Controllers/CookVC.swift
@@ -0,0 +1,25 @@
+//
+//  CookVC.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 8/31/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class CookVC: UIViewController {
+    //MARK: - UI Objects
+    
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - LifeCycle Methods
+    override func viewDidLoad() {
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    //MARK: - Methods
+    
+}

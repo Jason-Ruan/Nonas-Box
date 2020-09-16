f4df52bba8bcbf73109329ae0bfd9b1f84f49c94
Jason Ruan <jasonruan@pursuit.org>
2020-09-15 23:03:18 -0400

    Changes timerDisplayCount type and adds ui objects to TimerVC

--- Nonas Box/Nonas Box/View Controllers/TimerVC.swift
+++ Nonas Box/Nonas Box/View Controllers/TimerVC.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class TimerVC: UIViewController {
     //MARK: - UI Objects
+    lazy var timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = self.timerDisplayCount.description
+        return label
+    }()
     
+    lazy var startTimerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start", for: .normal)
+        return button
+    }()
+    
+    lazy var pauseTimerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Pause", for: .normal)
+        return button
+    }()
+    
+    lazy var resetTimerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        return button
+    }()
     
     //MARK: - Properties
     var timer = Timer()
-    var timerDisplayCount = 0
+    var timerDisplayCount = 0.0
     
     //MARK: - LifeCycle Methods
     override func viewDidLoad() {

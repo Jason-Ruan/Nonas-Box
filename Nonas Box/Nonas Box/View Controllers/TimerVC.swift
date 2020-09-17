9fd619845b7f0e409ba532d1dd6234d3b370199f
Jason Ruan <jasonruan@pursuit.org>
2020-09-16 22:42:50 -0400

    Edits properties of ui objects in TimerVC

--- Nonas Box/Nonas Box/View Controllers/TimerVC.swift
+++ Nonas Box/Nonas Box/View Controllers/TimerVC.swift
@@ -12,24 +12,32 @@ class TimerVC: UIViewController {
     //MARK: - UI Objects
     lazy var timerLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = self.timerDisplayCount.description
+        label.textAlignment = .center
+        label.font = UIFont(name: "Arial", size: 20)
         return label
     }()
     
-    lazy var startTimerButton: UIButton = {
+    lazy var toggleTimerButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Start", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        button.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
         return button
     }()
     
     lazy var pauseTimerButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Pause", for: .normal)
         return button
     }()
     
     lazy var resetTimerButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Reset", for: .normal)
         return button
     }()

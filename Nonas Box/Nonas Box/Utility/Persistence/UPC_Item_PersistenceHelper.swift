5a9e874f5f73e16b69c290cb600cb5be18e6c0f2
Jason Ruan <jasonruan@pursuit.org>
2020-09-25 18:36:17 -0400

    Adds persistence functions to UPC_Item_PersistenceHelper

--- Nonas Box/Nonas Box/Utility/Persistence/UPC_Item_PersistenceHelper.swift
+++ Nonas Box/Nonas Box/Utility/Persistence/UPC_Item_PersistenceHelper.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class UPC_Item_PersistenceHelper {
+    //MARK: - Persistence Methods
+    func save(key: String, item: UPC_Item) throws {
+        try persistenceHelper.save(key: key, newElement: item)
+    }
+    
+    func delete(barcode: String) throws {
+        try persistenceHelper.delete(key: barcode)
+    }
+    
+    func getSavedItems() throws -> [String : UPC_Item] {
+        return try persistenceHelper.getObjects()
+    }
+    
+    //MARK: - Singleton Properties
+    static let manager = UPC_Item_PersistenceHelper()
+    private let persistenceHelper = PersistenceHelper<UPC_Item>(fileName: "UPC_Items.plist")
+    private init() {}
+}

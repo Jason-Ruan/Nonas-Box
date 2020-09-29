dd1a1781d06d8c1fda4dd371f8b4cb338d057abe
Jason Ruan <jasonruan@pursuit.org>
2020-09-29 18:37:34 -0400

    Adds InventoryVC

---
+++ Nonas Box/Nonas Box/View Controllers/InventoryVC.swift
@@ -0,0 +1,35 @@
+//
+//  InventoryVC.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 9/29/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class InventoryVC: UIViewController {
+    
+    //MARK: - Properties
+    var inventory: [UPC_Item] = []
+    
+    
+    //MARK: - LifeCycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadInventory()
+    }
+    
+    
+    //MARK: - Private Functions
+    private func loadInventory() {
+        do {
+            inventory = try Array( UPC_Item_PersistenceHelper.manager.getSavedItems().values)
+        } catch {
+            print("Unable to load save items")
+        }
+    }
+    
+    
+}
+

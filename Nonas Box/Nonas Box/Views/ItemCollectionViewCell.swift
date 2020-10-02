f0a49f43a8a7e386068c8d6e508d595913f7d758
Jason Ruan <jasonruan@pursuit.org>
2020-10-01 21:27:19 -0400

    Adds subviews and configuration to ItemCollectionViewCell

--- Nonas Box/Nonas Box/Views/ItemCollectionViewCell.swift
+++ Nonas Box/Nonas Box/Views/ItemCollectionViewCell.swift
@@ -6,4 +6,82 @@
 //  Copyright © 2020 Jason Ruan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - UI Objects
+    lazy var itemImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    lazy var itemNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    
+    //MARK: - Properties
+    var item: UPC_Item! {
+        didSet {
+            configureCell(forItem: item)
+        }
+    }
+    
+    
+    //MARK: - Initializerss
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        constrainItemImageView()
+        constrainItemNameLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: - Functions
+    private func configureCell(forItem item: UPC_Item) {
+        itemNameLabel.text = item.title
+        
+        guard let barcode = item.barcode, let imageURLs = item.images else { return }
+        
+        UPC_ItemDB_Client.manager.getItemImage(barcode: barcode, imageURLs: imageURLs) { (result) in
+            switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let image):
+                    self.itemImageView.image = image
+            }
+        }
+    }
+    
+    
+    //MARK: - Constraints
+    private func constrainItemImageView() {
+        contentView.addSubview(itemImageView)
+        NSLayoutConstraint.activate([
+            itemImageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+            itemImageView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
+            itemImageView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            itemImageView.heightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.heightAnchor, multiplier: 0.75)
+        ])
+    }
+    
+    private func constrainItemNameLabel() {
+        contentView.addSubview(itemNameLabel)
+        NSLayoutConstraint.activate([
+            itemNameLabel.topAnchor.constraint(equalTo: itemImageView.bottomAnchor),
+            itemNameLabel.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
+            itemNameLabel.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            itemNameLabel.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+}

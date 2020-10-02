5dca55c03e72c596a01f2bba35f3124441ff9902
Jason Ruan <jasonruan@pursuit.org>
2020-10-01 21:26:22 -0400

    Adds itemCollectionView to InventoryVC

--- Nonas Box/Nonas Box/View Controllers/InventoryVC.swift
+++ Nonas Box/Nonas Box/View Controllers/InventoryVC.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class InventoryVC: UIViewController {
     
+    //MARK: - UI Objects
+    lazy var itemCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.safeAreaLayoutGuide.layoutFrame.width / 3,
+                                 height: view.safeAreaLayoutGuide.layoutFrame.height / 5)
+        layout.minimumLineSpacing = 10
+        
+        let cv = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        cv.register(ItemCollectionViewCell.self, forCellWithReuseIdentifier: "itemCell")
+        cv.backgroundColor = .clear
+        cv.layer.borderWidth = 5
+        cv.layer.borderColor = UIColor.black.cgColor
+        cv.dataSource = self
+        cv.delegate = self
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    
     //MARK: - Properties
-    var inventory: [UPC_Item] = []
+    var inventory: [UPC_Item] = [] {
+        didSet {
+            itemCollectionView.reloadData()
+        }
+    }
     
     
     //MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        view.addSubview(itemCollectionView)
         loadInventory()
     }
     
@@ -34,3 +59,17 @@ class InventoryVC: UIViewController {
     
 }
 
+
+extension InventoryVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return inventory.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as? ItemCollectionViewCell else { return UICollectionViewCell() }
+        cell.item = inventory[indexPath.row]
+        return cell
+    }
+    
+    
+}

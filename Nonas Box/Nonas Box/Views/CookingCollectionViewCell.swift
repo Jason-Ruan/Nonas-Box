11a88a46d3389a87e7869666d3cf9cfedb91be70
Jason Ruan <jasonruan@pursuit.org>
2020-10-09 23:55:55 -0400

    Adds CookingCollectionViewCell and InstructionCollectionViewCell

---
+++ Nonas Box/Nonas Box/Views/CookingCollectionViewCell.swift
@@ -0,0 +1,91 @@
+//
+//  CookingCollectionViewCell.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 10/9/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class CookingCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - UI Objects
+    lazy var recipeImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    lazy var recipeNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var instructionCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: contentView.frame, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    
+    // MARK: - Properties
+    var recipe: RecipeDetails! {
+        didSet {
+            configureCell(forRecipe: recipe)
+        }
+    }
+    
+    
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.layer.cornerRadius = 15
+        layer.shadowRadius = 10
+        layer.shadowOffset = CGSize(width: 10, height: 10)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Private Methods
+    private func configureCell(forRecipe recipe: RecipeDetails) {
+        if let imageURL = recipe.image {
+            ImageHelper.shared.getImage(url: imageURL) { (result) in
+                switch result {
+                    case .success(let image):
+                        self.recipeImageView.image = image
+                    case .failure(let error):
+                        print("Could not find image for \(recipe.title ?? "this recipe"), error: \(error)")
+                }
+            }
+        }
+        
+        self.recipeNameLabel.text = recipe.title
+    }
+    
+}
+
+
+// MARK: - CollectionView Methods
+extension CookingCollectionViewCell: UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recipe.analyzedInstructions?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "instructionCell", for: indexPath) as? InstructionCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
+    
+    
+}
---
+++ Nonas Box/Nonas Box/Views/InstructionCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstructionCollectionViewCell.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 10/9/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class InstructionCollectionViewCell: UICollectionViewCell {
+    
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}

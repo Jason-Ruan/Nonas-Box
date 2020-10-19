10ff9cf233d27ee7b3a821375bbe2f97d322ccf7
Jason Ruan <jasonruan@pursuit.org>
2020-10-18 20:00:22 -0400

    Adds StepByStepInstructionTableViewCell

---
+++ Nonas Box/Nonas Box/Views/StepByStepInstructionTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  StepByStepInstructionTableViewCell.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 10/18/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import Foundation

940b8361a2f65e6031fc842060bd302de1938cac
Jason Ruan <jasonruan@pursuit.org>
2020-11-13 23:20:50 -0500

    Changes font size for recipeBlurbInfoLabel in RecipeDetailVC

---
+++ Nonas Box/Nonas Box/Views/Labels/RecipeBlurbInfoLabel.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeBlurbInfoLabel.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 11/12/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class RecipeBlurbInfoLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

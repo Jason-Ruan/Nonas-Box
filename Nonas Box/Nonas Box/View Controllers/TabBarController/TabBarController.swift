63497add165a6e3037e511ba69d5adc477a6d347
Jason Ruan <jasonruan@pursuit.org>
2020-11-25 21:27:47 -0500

    Configures TabBarController viewControllers

--- Nonas Box/Nonas Box/View Controllers/TabBarController/TabBarController.swift
+++ Nonas Box/Nonas Box/View Controllers/TabBarController/TabBarController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureViewControllers()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Functions
+    private func configureViewControllers() {
+        let searchVC = UINavigationController(rootViewController: SearchRecipesOnlineVC())
+        let cookVC = UINavigationController(rootViewController: CookVC())
+        let timerVC = TimerVC()
+        let myStuffVC = UINavigationController(rootViewController: MyStuffVC())
+        
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "doc.text.magnifyingglass"), tag: 0)
+        cookVC.tabBarItem = UITabBarItem(title: "Cook", image: UIImage(systemName: "dial.min.fill"), tag: 1)
+        timerVC.tabBarItem = UITabBarItem(title: "Timer", image: UIImage(systemName: "timer"), tag: 2)
+        myStuffVC.tabBarItem = UITabBarItem(title: "Pantry", image: UIImage(systemName: "tray.2.fill"), tag: 3)
+                
+        viewControllers = [searchVC, cookVC, timerVC, myStuffVC]
     }
-    */
 
 }

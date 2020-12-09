a7e1d0c2542440b8e0a97ad2601b6237db437fb6
Jason Ruan <jasonruan@pursuit.org>
2020-12-09 00:04:35 -0500

    Adds test for recipe fetching service

--- Nonas Box/Nonas BoxTests/Nonas_BoxTests.swift
+++ Nonas Box/Nonas BoxTests/Nonas_BoxTests.swift
@@ -19,16 +19,27 @@ class Nonas_BoxTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testRecipeFetchingService() {
+        let expectation = self.expectation(description: "The SpoonacularAPIClient should complete asynchronous call and successfully return recipes")
+        SpoonacularAPIClient.manager.getRecipes(query: "chicken") { (result) in
+            switch result {
+                case .failure(let error):
+                    XCTFail("Asynchronous call to Spoonacular endpoint failed to complete, with error: \(error)")
+                case .success(let spoonacularResults):
+                    guard let fetchedRecipes = spoonacularResults.results else {
+                        return XCTFail("Asynchronous call to Spoonacular endpoint completed successfully but did not return recipes for query")
+                    }
+                    XCTAssert(!fetchedRecipes.isEmpty, "Spoonacular call should return at least 1 recipe for provided query")
+            }
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 5) { (error) in
+            if let error = error {
+                XCTFail("Test for Spoonacular recipe fetch timed out with error: \(error)")
+            }
+        }
+        
     }
 
 }

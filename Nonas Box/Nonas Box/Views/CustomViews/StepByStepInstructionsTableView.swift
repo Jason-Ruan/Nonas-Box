da393368fc7246065fab1907bdf71b98a2917877
Jason Ruan <jasonruan@pursuit.org>
2020-12-12 18:53:08 -0500

    Adds StepByStepInstructionsTableView

--- Nonas Box/Nonas Box/Views/CustomViews/StepByStepInstructionsTableView.swift
+++ Nonas Box/Nonas Box/Views/CustomViews/StepByStepInstructionsTableView.swift
@@ -9,13 +9,61 @@
 import UIKit
 
 class StepByStepInstructionsTableView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: UI Objects
+    public lazy var underlinedSegmentedControl: UnderlinedSegmentedControl = {
+        let underlinedSC = UnderlinedSegmentedControl(items: ["Ingredients", "Directions"])
+        underlinedSC.segmentedControl.addTarget(self, action: #selector(jumpToSection), for: .valueChanged)
+        return underlinedSC
+    }()
+    
+    public lazy var tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .grouped)
+        tv.backgroundColor = .clear
+        tv.bounces = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "ingredientCell")
+        tv.register(StepByStepInstructionTableViewCell.self, forCellReuseIdentifier: StepByStepInstructionTableViewCell.identifier)
+        return tv
+    }()
+    
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Private Functions
+    private func setUpViews() {
+        addSubview(underlinedSegmentedControl)
+        underlinedSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            underlinedSegmentedControl.topAnchor.constraint(equalTo: topAnchor),
+            underlinedSegmentedControl.leadingAnchor.constraint(equalTo: leadingAnchor),
+            underlinedSegmentedControl.trailingAnchor.constraint(equalTo: trailingAnchor),
+            underlinedSegmentedControl.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15)
+        ])
+        
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: underlinedSegmentedControl.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    
+    // MARK: - Private ObjC Functions
+    @objc
+    private func jumpToSection() {
+        tableView.scrollToRow(at: IndexPath(row: 0, section: underlinedSegmentedControl.segmentedControl.selectedSegmentIndex), at: .top, animated: true)
+    }
 
 }

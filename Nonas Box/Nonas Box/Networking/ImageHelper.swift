0161a7cb1029a7391e8f99b4072dab0bf7699901
Jason Ruan <jasonruan@pursuit.org>
2020-12-12 18:35:51 -0500

    Edits getImage func in ImageHelper

--- Nonas Box/Nonas Box/Networking/ImageHelper.swift
+++ Nonas Box/Nonas Box/Networking/ImageHelper.swift
@@ -17,31 +17,41 @@ class ImageHelper {
     
     //MARK: - Caching Properties
     
-    let imageCache = NSCache<NSString, UIImage>()
+    private let imageCache = NSCache<NSString, UIImage>()
     
     // MARK: - Instance Methods
     
-    func getImage(url: URL, completionHandler: @escaping (Result<UIImage, AppError>) -> ()) {
+    func getImage(cacheKey: String? = nil, urls: [URL], completionHandler: @escaping (Result<UIImage, AppError>) -> ()) {
+        
+        guard let url = urls.first else {
+            return completionHandler(.failure(.badURL))
+        }
         
         //Checks cache to see if image was previously stored, before making network call
-        if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
+        if let cachedImage = imageCache.object(forKey: (cacheKey ?? url.absoluteString) as NSString) {
             completionHandler(.success(cachedImage))
         } else {
             
             //Makes network call to attempt to return image via url if available
-            NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { [weak self] (result) in
                 DispatchQueue.main.async {
                     switch result {
                         case .failure(let error):
-                            completionHandler(.failure(error))
+                            let remainingURLs: [URL] = Array(urls.dropFirst())
+                            if !remainingURLs.isEmpty {
+                                self?.getImage(cacheKey: cacheKey, urls: remainingURLs, completionHandler: completionHandler)
+                            } else {
+                                completionHandler(.failure(error))
+                            }
+                            
                         case .success(let imageData):
                             guard let image = UIImage(data: imageData) else {
                                 completionHandler(.failure(.notAnImage))
                                 return
                             }
                             //Cache image for future use and return it
-                            DispatchQueue.global().async {
-                                self.imageCache.setObject(image, forKey: url.absoluteString as NSString)
+                            DispatchQueue.global().async { [weak self] in
+                                self?.imageCache.setObject(image, forKey: (cacheKey ?? url.absoluteString) as NSString)
                             }
                             completionHandler(.success(image))
                     }

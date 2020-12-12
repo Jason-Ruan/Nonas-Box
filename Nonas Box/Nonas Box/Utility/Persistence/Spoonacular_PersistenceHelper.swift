76b5dc3682ead84e14f6ab8a4fa462802bc04948
Jason Ruan <jasonruan@pursuit.org>
2020-12-12 18:36:08 -0500

    Adds persistenceHelper for Spoonacular

--- Nonas Box/Nonas Box/Utility/Persistence/Spoonacular_PersistenceHelper.swift
+++ Nonas Box/Nonas Box/Utility/Persistence/Spoonacular_PersistenceHelper.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class Spoonacular_PersistenceHelper {
+    //MARK: - Persistence Methods
+    func save(key: String, item: RecipeDetails) throws {
+        try persistenceHelper.save(key: key, newElement: item)
+    }
+    
+    func delete(barcode: String) throws {
+        try persistenceHelper.delete(key: barcode)
+    }
+    
+    func getSavedItems() throws -> [String : RecipeDetails] {
+        return try persistenceHelper.getObjects()
+    }
+    
+    //MARK: - Singleton Properties
+    static let manager = Spoonacular_PersistenceHelper()
+    private let persistenceHelper = PersistenceHelper<RecipeDetails>(fileName: "SpoonacularRecipeDetails.plist")
+    private init() {}
+}

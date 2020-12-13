e67ca5887b58f288e9d2711ee0d79a196003609a
Jason Ruan <jasonruan@pursuit.org>
2020-12-13 17:32:06 -0500

    Edits ButtonStackView

--- Nonas Box/Nonas Box/Views/StackViews/ButtonStackView.swift
+++ Nonas Box/Nonas Box/Views/StackViews/ButtonStackView.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class ButtonStackView: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Public Properties
+    public let bookmarkButton: UIButton
+    public let weblinkButton: UIButton
+    
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        let bookmarkButton = UIButton(type: .system)
+        bookmarkButton.tintColor = .systemRed
+        bookmarkButton.setTitle("Bookmark", for: .normal)
+        bookmarkButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        self.bookmarkButton = bookmarkButton
+        
+        let webLinkButton = UIButton(type: .system)
+        webLinkButton.setImage(UIImage(systemName: "safari.fill"), for: .normal)
+        webLinkButton.setTitle("Source", for: .normal)
+        webLinkButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        self.weblinkButton = webLinkButton
+        
+        super.init(frame: frame)
+        addArrangedSubview(bookmarkButton)
+        addArrangedSubview(webLinkButton)
+        
+        axis = .horizontal
+        distribution = .fillEqually
+        spacing = 5
     }
-    */
-
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }

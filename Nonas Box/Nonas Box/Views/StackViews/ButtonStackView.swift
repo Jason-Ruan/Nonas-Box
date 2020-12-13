b68a71c111188fdc6ce82200f35c009b966cebf0
Jason Ruan <jasonruan@pursuit.org>
2020-12-13 17:25:21 -0500

    Adds ButtonStackView to Views

---
+++ Nonas Box/Nonas Box/Views/StackViews/ButtonStackView.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonStackView.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 12/13/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ButtonStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

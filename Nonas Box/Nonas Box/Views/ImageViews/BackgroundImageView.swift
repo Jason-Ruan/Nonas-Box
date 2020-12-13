66cfc6197c618d4c74d891204fd5082e788dbc62
Jason Ruan <jasonruan@pursuit.org>
2020-12-13 16:51:37 -0500

    Adds various view files and folders

---
+++ Nonas Box/Nonas Box/Views/ImageViews/BackgroundImageView.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundImageView.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 12/13/20.
+//  Copyright © 2020 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

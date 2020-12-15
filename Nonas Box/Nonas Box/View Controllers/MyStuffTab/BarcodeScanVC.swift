622b4e5842a88bc56b48e4cf1e68e1243529e128
Jason Ruan <jasonruan@pursuit.org>
2020-12-15 00:17:10 -0500

    Makes navigation bar translucent in various VCs

--- Nonas Box/Nonas Box/View Controllers/MyStuffTab/BarcodeScanVC.swift
+++ Nonas Box/Nonas Box/View Controllers/MyStuffTab/BarcodeScanVC.swift
@@ -52,6 +52,7 @@ class BarcodeScanVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureNavigationBarForTranslucence()
         requestAVCapturePermissions()
     }
     
--- Nonas Box/Nonas Box/View Controllers/MyStuffTab/InventoryVC.swift
+++ Nonas Box/Nonas Box/View Controllers/MyStuffTab/InventoryVC.swift
@@ -32,7 +32,7 @@ class InventoryVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.isHidden = false
+        configureNavigationBarForTranslucence()
         constrainItemCollectionView()
         loadInventory()
     }

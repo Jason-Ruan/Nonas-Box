cc04ed0b4032bcc0ba57bfad2fe50f1facd3785d
Jason Ruan <jasonruan@pursuit.org>
2020-12-21 21:58:06 -0500

    Edits cornerRadius of label in RecipeCollectionViewCell

--- Nonas Box/Nonas Box/Views/CollectionViewCells/RecipeCollectionViewCell.swift
+++ Nonas Box/Nonas Box/Views/CollectionViewCells/RecipeCollectionViewCell.swift
@@ -30,7 +30,7 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         label.adjustsFontSizeToFitWidth = true
         label.textAlignment = .center
         label.backgroundColor = #colorLiteral(red: 0.2295365632, green: 0.2428716421, blue: 0.2767262459, alpha: 0.5)
-        label.layer.cornerRadius = 25
+        label.layer.cornerRadius = 15
         label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         label.clipsToBounds = true
         label.translatesAutoresizingMaskIntoConstraints = false

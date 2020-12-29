596b508a887b6554391a5399b4c948d1d7b08ffa
Jason Ruan <jasonruan@pursuit.org>
2020-12-28 20:30:43 -0500

    Sets tabBar to not be translucent

--- Nonas Box/Nonas Box/View Controllers/TabBarController/TabBarController.swift
+++ Nonas Box/Nonas Box/View Controllers/TabBarController/TabBarController.swift
@@ -14,6 +14,7 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewControllers()
+        tabBar.isTranslucent = false
     }
     
 

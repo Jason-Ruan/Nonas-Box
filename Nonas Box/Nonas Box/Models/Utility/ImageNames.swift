465f015af3a97480dcbba12a268056c12e54f9a7
Jason Ruan <jasonruan@pursuit.org>
2021-01-30 23:07:30 -0500

    Adds ImageNames file

---
+++ Nonas Box/Nonas Box/Models/Utility/ImageNames.swift
@@ -0,0 +1,15 @@
+//
+//  ImageName.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 1/24/21.
+//  Copyright © 2021 Jason Ruan. All rights reserved.
+//
+
+import Foundation
+
+enum ImageNames: String {
+    case darkTableBottomIngredients = "dark-table-ingredients"
+    case lightTableBottomIngredientss = "light-table-ingredients"
+    case foodBorderEmptyCenter = "food-border"
+}

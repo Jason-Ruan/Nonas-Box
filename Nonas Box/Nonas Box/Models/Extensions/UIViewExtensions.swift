deee3c80e518873c4ab27fec8fbfea48d4849f29
Jason Ruan <jasonruan@pursuit.org>
2021-02-06 17:50:53 -0500

    Adds files for UIViewExtensions and SSystemImageNames model

---
+++ Nonas Box/Nonas Box/Models/Extensions/UIViewExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtensions.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 2/3/21.
+//  Copyright © 2021 Jason Ruan. All rights reserved.
+//
+
+import Foundation
---
+++ Nonas Box/Nonas Box/Models/Utility/SystemImage.swift
@@ -0,0 +1,9 @@
+//
+//  SystemImage.swift
+//  Nonas Box
+//
+//  Created by Jason Ruan on 2/6/21.
+//  Copyright © 2021 Jason Ruan. All rights reserved.
+//
+
+import Foundation

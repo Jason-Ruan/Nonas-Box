5213a8cca2a552e0f539a60da25b478593de178a
Jason Ruan <jasonruan@pursuit.org>
2021-02-06 17:58:33 -0500

    Edits searchController code in CookVC

--- Nonas Box/Nonas Box/View Controllers/CookTab/CookVC.swift
+++ Nonas Box/Nonas Box/View Controllers/CookTab/CookVC.swift
@@ -32,35 +32,34 @@ class CookVC: UIViewController {
     
     //MARK: - LifeCycle Methods
     override func viewDidLoad() {
-        configureBackgroundColor()
+        view.addGradientLayer(colors: [#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), .white])
         configureSearchController()
         configureNavigationBarForTranslucence()
         addSubviews()
-        recipes = fetchBookmarkedRecipes()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white]
-        recipes = fetchBookmarkedRecipes()
+//        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white]
+//        if let searchBarText = searchController.searchBar.text?.lowercased(), searchBarText.count > 0 {
+//            recipes = fetchBookmarkedRecipes().filter { ($0.title?.lowercased().contains(searchBarText) ?? false) }
+//        } else {
+//            recipes = fetchBookmarkedRecipes()
+//        }
+        recipes = fetchBookmarkedRecipes(withTitleContaining: searchController.searchBar.text,
+                                         filteredBy: (1...2).contains(searchController.searchBar.selectedScopeButtonIndex) ?
+                                            RecipeFilterCriteria.init(rawValue: searchController.searchBar.selectedScopeButtonIndex) : nil)
+        
     }
     
     
     //MARK: - Private Methods
-    private func configureBackgroundColor() {
-        let gradientBackgroundLayer = CAGradientLayer()
-        gradientBackgroundLayer.colors = [#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1).cgColor, UIColor.white.cgColor]
-        gradientBackgroundLayer.frame = view.bounds
-        view.layer.insertSublayer(gradientBackgroundLayer, at: 0)
-    }
-    
     private func configureSearchController() {
         searchController = UISearchController(searchResultsController: nil)
         searchController.automaticallyShowsScopeBar = true
         searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.scopeButtonTitles = ["All", "Bookmarked", "My Recipes"]
+        searchController.searchBar.scopeButtonTitles = ["All", "Quick (<1hr)", "Steady (1hr+)"]
         searchController.searchBar.delegate = self
-        
-        editSearchbarAttributedPlaceholder(withText: "Looking for a specific recipe?", andColor: .white)
+        searchController.searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "Looking for a specific recipe?", attributes: [.foregroundColor : UIColor.white])
         
         navigationItem.title = "Nona's Box"
         navigationItem.searchController = searchController
@@ -77,19 +76,28 @@ class CookVC: UIViewController {
         ])
     }
     
-    private func fetchBookmarkedRecipes() -> [RecipeDetails] {
+    private func fetchBookmarkedRecipes(withTitleContaining term: String? = nil, filteredBy criteria: RecipeFilterCriteria? = nil) -> [RecipeDetails] {
+        var recipes: [RecipeDetails] = []
+        
         do {
-            return try Spoonacular_PersistenceHelper.manager.getSavedRecipes()
+            recipes = try Spoonacular_PersistenceHelper.manager.getSavedRecipes()
         } catch {
             print(error)
-            return []
         }
+        
+        if let term = term?.lowercased(), term.count > 0 {
+            recipes = recipes.filter {
+                guard let title = $0.title?.lowercased(), let min = $0.readyInMinutes else { return false }
+                switch criteria {
+                    case .underAnHour:              return title.contains(term) && min < 60
+                    case .hourOrMore:               return title.contains(term) && min >= 60
+                    default:                        return title.contains(term)
+                }
+            }
+        }
+        
+        return recipes
     }
-    
-    private func editSearchbarAttributedPlaceholder(withText text: String, andColor color: UIColor) {
-        searchController.searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: text, attributes: [.foregroundColor : color])
-    }
-    
     
 }
 
@@ -120,44 +128,42 @@ extension CookVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 }
 
 
-// MARK: - UISearchResults Methods
-extension CookVC: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        print("Updating search results")
-    }
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        editSearchbarAttributedPlaceholder(withText: "Looking for a specific recipe?", andColor: .systemGray2)
-    }
-    
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        editSearchbarAttributedPlaceholder(withText: "Looking for a specific recipe?", andColor: .white)
-    }
-}
-
-
 // MARK: - UISearchBar Methods
 extension CookVC: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        recipes = fetchBookmarkedRecipes().filter {
-            guard let recipeTitle = $0.title?.lowercased(), let query = searchBar.text?.lowercased() else { return false }
-            return recipeTitle.contains(query)
-        }
+        recipes = fetchBookmarkedRecipes(withTitleContaining: searchBar.text,
+                                         filteredBy: (1...2).contains(searchBar.selectedScopeButtonIndex) ?
+                                                        RecipeFilterCriteria.init(rawValue: searchBar.selectedScopeButtonIndex) : nil)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard searchText.count > 0 else {
-            recipes = fetchBookmarkedRecipes()
-            return
-        }
-        recipes = fetchBookmarkedRecipes().filter {
-            guard let recipeTitle = $0.title?.lowercased() else { return false }
-            return recipeTitle.contains(searchText.lowercased())
-        }
+        recipes = fetchBookmarkedRecipes(withTitleContaining: searchText,
+                                         filteredBy: (1...2).contains(searchBar.selectedScopeButtonIndex) ?
+                                                        RecipeFilterCriteria.init(rawValue: searchBar.selectedScopeButtonIndex) : nil)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         recipes = fetchBookmarkedRecipes()
     }
     
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        switch selectedScope {
+            case 0:                 recipes = fetchBookmarkedRecipes(withTitleContaining: searchBar.text)
+            case 1:                 recipes = fetchBookmarkedRecipes(withTitleContaining: searchBar.text, filteredBy: .underAnHour)
+            case 2:                 recipes = fetchBookmarkedRecipes(withTitleContaining: searchBar.text, filteredBy: .hourOrMore)
+            default:                return
+        }
+    }
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.selectedScopeButtonIndex = 0
+        recipes = fetchBookmarkedRecipes(withTitleContaining: searchBar.text)
+        return true
+    }
+    
+}
+
+fileprivate enum RecipeFilterCriteria: Int {
+    case underAnHour = 1
+    case hourOrMore = 2
 }

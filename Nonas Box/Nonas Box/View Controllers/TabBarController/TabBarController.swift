0cf0f1660d6d10e728b5a6b8239f4ed3474003ba
Jason Ruan <jasonruan@pursuit.org>
2021-03-06 18:43:27 -0500

    Refactors TabBarController and makes various visual edits

--- Nonas Box/Nonas Box/View Controllers/TabBarController/TabBarController.swift
+++ Nonas Box/Nonas Box/View Controllers/TabBarController/TabBarController.swift
@@ -13,40 +13,83 @@ class TabBarController: UITabBarController {
     // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureViewControllers()
+        configureTabBarItemsVCs(embedWithNavigationController: [.search, .cook, .shopping, .pantry])
         tabBar.isTranslucent = false
-        tabBar.tintColor = .white
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0.7459085584, blue: 0.4750729799, alpha: 1)
         tabBar.barTintColor = .black
         tabBar.unselectedItemTintColor = #colorLiteral(red: 0.4587794542, green: 0.463016808, blue: 0.4736304283, alpha: 0.8525791952)
-        
-        guard let customFont = UIFont(name: Fonts.wideMarker.rawValue, size: 10.5) else { return }
-        UITabBarItem.appearance().setTitleTextAttributes([.font : customFont], for: .normal)
+        setTabBarTitleFont(font: .wideMarker, size: 10.5)
     }
     
 
     // MARK: - Private Functions
-    private func configureViewControllers() {
-        let searchVC = UINavigationController(rootViewController: SearchRecipesOnlineVC())
-        let cookVC = UINavigationController(rootViewController: CookVC())
-        let timerVC = TimerVC()
-        let myStuffVC = UINavigationController(rootViewController: MyStuffVC())
-        let shoppingVC = UINavigationController(rootViewController: UIViewController())
-        
-        searchVC.tabBarItem = UITabBarItem(title: TabBarTitles.search.title, image: UIImage(systemName: .magnifyingglass), tag: 0)
-        cookVC.tabBarItem = UITabBarItem(title: TabBarTitles.cook.title, image: UIImage(systemName: .dial), tag: 1)
-        timerVC.tabBarItem = UITabBarItem(title: TabBarTitles.timer.title, image: UIImage(systemName: .timer), tag: 2)
-        shoppingVC.tabBarItem = UITabBarItem(title: TabBarTitles.shopping.title, image: UIImage(systemName: .bag), tag: 3)
-        myStuffVC.tabBarItem = UITabBarItem(title: TabBarTitles.pantry.title, image: UIImage(systemName: .trays), tag: 4)
-                        
-        viewControllers = [searchVC, cookVC, timerVC, shoppingVC, myStuffVC]
+    private func createTabBarItemVC(tabBarItemType: TabBarItemType) -> UIViewController {
+        let vc = tabBarItemType.viewController
+        vc.tabBarItem = UITabBarItem(title: tabBarItemType.title, image: tabBarItemType.image, tag: tabBarItemType.index)
+        return vc
     }
+    
+    private func configureTabBarItemsVCs(embedWithNavigationController rootViewControllers: Set<TabBarItemType>? = nil) {
+        if let rootViewControllers = rootViewControllers {
+            viewControllers = TabBarItemType.allCases.map {
+                if rootViewControllers.contains($0) {
+                    return UINavigationController(rootViewController: createTabBarItemVC(tabBarItemType: $0))
+                } else {
+                    return createTabBarItemVC(tabBarItemType: $0)
+                }
+            }
+        } else {
+            viewControllers = TabBarItemType.allCases.map { createTabBarItemVC(tabBarItemType: $0) }
+        }
+    }
+    
+    private func setTabBarTitleFont(font: Fonts, size: CGFloat) {
+        guard let customFont = UIFont(name: font.rawValue, size: size) else { return }
+        UITabBarItem.appearance().setTitleTextAttributes([.font : customFont], for: .normal)
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let itemTitle = item.title?.lowercased() else { return }
+        tabBar.tintColor = TabBarItemType.init(rawValue: itemTitle)?.colorScheme
+    }
+    
+    
 
 }
 
-fileprivate enum TabBarTitles: String {
+fileprivate enum TabBarItemType: String, CaseIterable {
     case search, cook, timer, shopping, pantry
     
-    var title: String {
-        rawValue.uppercased()
+    var title: String { rawValue.uppercased() }
+    var index: Int { Self.allCases.firstIndex(of: self) ?? 0 }
+    
+    var image: UIImage? {
+        switch self {
+            case .search:           return UIImage(systemName: .magnifyingglass)
+            case .cook:             return UIImage(systemName: .dial)
+            case .timer:            return UIImage(systemName: .timer)
+            case .shopping:         return UIImage(systemName: .bag)
+            case .pantry:           return UIImage(systemName: .trays)
+        }
+    }
+    
+    var colorScheme: UIColor {
+        switch self {
+            case .search:           return #colorLiteral(red: 1, green: 0.6408555508, blue: 0.365842253, alpha: 0.9022502369)
+            case .cook:             return #colorLiteral(red: 0.9539069533, green: 0.6485298276, blue: 0.5980203748, alpha: 1)
+            case .timer:            return #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+            case .shopping:         return #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+            case .pantry:           return #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        }
+    }
+    
+    var viewController: UIViewController {
+        switch self {
+            case .search:           return SearchRecipesOnlineVC()
+            case .cook:             return CookVC()
+            case .timer:            return TimerVC()
+            case .shopping:         return UIViewController()
+            case .pantry:           return MyStuffVC()
+        }
     }
 }

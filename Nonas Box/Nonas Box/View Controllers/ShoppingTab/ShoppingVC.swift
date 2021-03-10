ea28629b42628ee71928a1c7010ce9e236a3f9a6
Jason Ruan <jasonruan@pursuit.org>
2021-03-09 23:29:54 -0500

    Adds ShoppingVC

--- Nonas Box/Nonas Box/View Controllers/ShoppingTab/ShoppingVC.swift
+++ Nonas Box/Nonas Box/View Controllers/ShoppingTab/ShoppingVC.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class ShoppingVC: UIViewController {
+    
+    private lazy var shoppingListTableView: UITableView = {
+        let tv = UITableView(frame: view.safeAreaLayoutGuide.layoutFrame, style: .plain)
+        tv.backgroundColor = TabBarItemType.shopping.colorScheme
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "shoppingCell")
+        tv.dataSource = self
+        tv.delegate = self
+        return tv
+    }()
+    
+    public var recipeDetails: [RecipeDetails] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.overrideUserInterfaceStyle = .light
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.standardAppearance.largeTitleTextAttributes = [.font : UIFont(name: Fonts.handwriting.rawValue, size: 34)!]
+        
+        navigationItem.title = TabBarItemType.shopping.title.capitalized
+        view.addSubview(shoppingListTableView)
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ShoppingVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeDetails.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationController?.pushViewController(RecipeDetailVC(recipeDetails: recipeDetails[indexPath.row]), animated: true)
+    }
+    
 }

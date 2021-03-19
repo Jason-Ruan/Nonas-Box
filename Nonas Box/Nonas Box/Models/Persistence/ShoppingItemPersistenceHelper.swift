77e34e4863d31874cccbd04173cbdc5900dbbc4c
Jason Ruan <jasonruan@pursuit.org>
2021-03-19 00:21:24 -0400

    Adds ShoppingItemPersistenceHelper

--- Nonas Box/Nonas Box/Models/Persistence/ShoppingItemPersistenceHelper.swift
+++ Nonas Box/Nonas Box/Models/Persistence/ShoppingItemPersistenceHelper.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class ShoppingItemPersistenceHelper {
+    // MARK: - Persistence Methods
+    func save(key: String, item: ShoppingItem) throws {
+        try persistenceHelper.save(key: key, newElement: item)
+    }
+    
+    func delete(key: String? = nil, title: String? = nil) throws {
+        try persistenceHelper.delete(key: key ?? title ?? "")
+    }
+    
+    func deleteAll() throws {
+        try persistenceHelper.deleteAll()
+    }
+    
+    func getSavedItemsDictionary() throws -> [String : ShoppingItem] {
+        return try persistenceHelper.getObjects()
+    }
+    
+    func getSavedItems() throws -> [ShoppingItem] {
+        return try persistenceHelper.getObjects().values.sorted(by: { (item1, item2) -> Bool in
+            return item1.itemName < item2.itemName
+        })
+    }
+    
+    func contains(item: ShoppingItem) -> Bool {
+        do {
+            return try persistenceHelper.getObjects().values.map { $0 }.contains(item)
+        } catch {
+            return false
+        }
+    }
+    
+    static let manager = ShoppingItemPersistenceHelper()
+    private let persistenceHelper = PersistenceHelper<ShoppingItem>(fileName: "ShoppingItems.plist")
+    private init() {}
+}

8cdefb2683b1fa22550017985723f09c4627e76d
Jason Ruan <jasonruan@pursuit.org>
2021-03-19 00:22:06 -0400

    Adds more cases for SystemImageNames enum

--- Nonas Box/Nonas Box/Models/Utility/SystemImage.swift
+++ Nonas Box/Nonas Box/Models/Utility/SystemImage.swift
@@ -9,12 +9,18 @@
 import Foundation
 
 enum SystemImageNames: String {
-    case magnifyingglass, timer, photo, questionmark
+    case magnifyingglass, timer, photo, questionmark, plus, trash
     case bag = "bag"
     case compose = "square.and.pencil"
     case chevronUpFill = "chevron.up.circle.fill"
     case dial = "dial.max"
     case trays = "tray.2"
+    case minusCircle = "minus.circle"
     case photoFill = "photo.fill"
+    case plusCircle = "plus.circle"
+    case plusCircleFill = "plus.circle.fill"
+    case trashFill = "trash.fill"
+    case trashCircle = "trash.circle"
+    case trashCircleFill = "trash.circle.fill"
     case xmarkRectangleFill = "xmark.rectangle.fill"
 }

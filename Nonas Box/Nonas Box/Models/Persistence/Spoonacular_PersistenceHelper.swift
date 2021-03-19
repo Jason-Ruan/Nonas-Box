91e4b75491550760871c2a34344910fe6fe31cdc
Jason Ruan <jasonruan@pursuit.org>
2021-03-19 00:21:50 -0400

    Edits persistenceHelper for recipes api

--- Nonas Box/Nonas Box/Models/Persistence/Spoonacular_PersistenceHelper.swift
+++ Nonas Box/Nonas Box/Models/Persistence/Spoonacular_PersistenceHelper.swift
@@ -10,23 +10,28 @@ import Foundation
 
 class Spoonacular_PersistenceHelper {
     //MARK: - Persistence Methods
-    func save(recipeID: Int, recipeDetails: RecipeDetails) throws {
-        try persistenceHelper.save(key: recipeID.description, newElement: recipeDetails)
+    func save(recipeID: Int, recipeDetails: RecipeDetails, persistenceStorage: PersistenceStorage) throws {
+        try (persistenceStorage == .collection ? persistenceHelper : shoppingListPersistenceHelper).save(key: recipeID.description, newElement: recipeDetails)
     }
     
-    func delete(recipeID: Int) throws {
-        try persistenceHelper.delete(key: recipeID.description)
+    func delete(recipeID: Int, persistenceStorage: PersistenceStorage) throws {
+        try (persistenceStorage == .collection ? persistenceHelper : shoppingListPersistenceHelper).delete(key: recipeID.description)
     }
     
-    func getSavedRecipesDictionary() throws -> [String : RecipeDetails] {
-        return try persistenceHelper.getObjects()
+    func getSavedRecipesDictionary(from persistenceStorage: PersistenceStorage) throws -> [String : RecipeDetails] {
+        return try (persistenceStorage == .collection ? persistenceHelper : shoppingListPersistenceHelper).getObjects()
+        
     }
     
-    func getSavedRecipes() throws -> [RecipeDetails] {
-        return try persistenceHelper.getObjects().values.sorted(by: { (recipe1, recipe2) -> Bool in
+    func getSavedRecipes(persistenceStorage: PersistenceStorage) throws -> [RecipeDetails] {
+        return try (persistenceStorage == .collection ? persistenceHelper : shoppingListPersistenceHelper).getObjects().values.sorted(by: { (recipe1, recipe2) -> Bool in
             guard let title1 = recipe1.title, let title2 = recipe2.title else { return false }
             return title1 < title2
         })
+    }
+    
+    func getRecipe(recipeID: Int? = nil, recipeName: String? = nil) throws -> RecipeDetails? {
+        return try persistenceHelper.getObjects()[recipeID != nil ? recipeID!.description : recipeName ?? ""]
     }
     
     func checkIsSaved(forRecipeID recipeID: Int) throws -> Bool {
@@ -36,5 +41,6 @@ class Spoonacular_PersistenceHelper {
     //MARK: - Singleton Properties
     static let manager = Spoonacular_PersistenceHelper()
     private let persistenceHelper = PersistenceHelper<RecipeDetails>(fileName: "SpoonacularRecipeDetails.plist")
+    private let shoppingListPersistenceHelper = PersistenceHelper<RecipeDetails>(fileName: "RecipeDetailsShoppingList.plist")
     private init() {}
 }

d3395ff0f0a7b5c0dd823531db5c5ceca361147e
Jason Ruan <jasonruan@pursuit.org>
2021-05-27 00:53:00 -0400

    Refactors RecipeCollectionViewCell and prepares it for reuse in CookVC

--- Nonas Box/Nonas Box/Views/CollectionViewCells/RecipeCollectionViewCell.swift
+++ Nonas Box/Nonas Box/Views/CollectionViewCells/RecipeCollectionViewCell.swift
@@ -20,7 +20,8 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         iv.tintColor = #colorLiteral(red: 1, green: 0.687940836, blue: 0.5207877159, alpha: 0.8489672517)
         iv.contentMode = .scaleAspectFill
         iv.layer.cornerRadius = layer.cornerRadius
-        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.kf.indicatorType = .activity
+        (iv.kf.indicator?.view as? UIActivityIndicatorView)?.color = #colorLiteral(red: 0.8859762549, green: 0.6057382822, blue: 0.4648851752, alpha: 1)
         return iv
     }()
     
@@ -29,7 +30,6 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         label.backgroundColor = #colorLiteral(red: 0.2295365632, green: 0.2428716421, blue: 0.2767262459, alpha: 0.5)
         label.layer.cornerRadius = 15
         label.clipsToBounds = true
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -38,7 +38,6 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         iv.isHidden = true
         iv.tintColor = .red
         iv.contentMode = .scaleAspectFill
-        iv.translatesAutoresizingMaskIntoConstraints = false
         iv.addGradientLayer(colors: [.yellow, .red])
         return iv
     }()
@@ -54,6 +53,13 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    public var recipeDetails: RecipeDetails! {
+        didSet {
+            guard let recipeDetails = self.recipeDetails else { return }
+            configureCell(recipeDetails: recipeDetails)
+        }
+    }
+    
     public static var identifier: String {
         return String(describing: self)
     }
@@ -65,8 +71,8 @@ class RecipeCollectionViewCell: UICollectionViewCell {
         super.init(frame: frame)
         backgroundColor = .white
         clipsToBounds = true
-        layer.cornerRadius = 25
-        layer.borderWidth = 1.25
+        layer.cornerRadius = 20
+        layer.borderWidth = 0.75
         layer.borderColor = UIColor.systemYellow.cgColor
         setUpCell()
     }
@@ -80,47 +86,48 @@ class RecipeCollectionViewCell: UICollectionViewCell {
     
     private func setUpCell() {
         contentView.addSubview(foodImage)
+        foodImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            foodImage.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
-            foodImage.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor),
-            foodImage.widthAnchor.constraint(equalToConstant: contentView.bounds.width),
-            foodImage.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor)
+            foodImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            foodImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            foodImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            foodImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
         
         contentView.addSubview(foodInfoLabel)
+        foodInfoLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            foodInfoLabel.heightAnchor.constraint(equalToConstant: contentView.safeAreaLayoutGuide.layoutFrame.height / 3),
-            foodInfoLabel.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            foodInfoLabel.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant:  -10),
-            foodInfoLabel.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+            foodInfoLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+            foodInfoLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            foodInfoLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant:  -10),
+            foodInfoLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
         ])
         
         contentView.addSubview(bookmarkedImageView)
+        bookmarkedImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            bookmarkedImageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
-            bookmarkedImageView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -(contentView.safeAreaLayoutGuide.layoutFrame.width / 7)),
-            bookmarkedImageView.heightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.heightAnchor, multiplier: 0.15)
+            bookmarkedImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            bookmarkedImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -(contentView.frame.width / 7)),
+            bookmarkedImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.15)
         ])
         
-    }
-    
-    private func configureCell(recipe: Recipe) {
-        loadImage(recipe: recipe)
-        foodInfoLabel.configureAttributedText(title: recipe.title, servings: recipe.servings, readyInMinutes: recipe.readyInMinutes)
-    }
-    
-    private func loadImage(recipe: Recipe) {
         layoutIfNeeded()
-        
-        guard let imageURL = recipe.imageURL else {
-            foodImage.image = UIImage(systemName: .photoFill)
-            return
+    }
+    
+    private func configureCell(recipe: Recipe? = nil, recipeDetails: RecipeDetails? = nil) {
+        if let recipe = recipe {
+            loadImage(url: recipe.imageURL)
+            foodInfoLabel.configureAttributedText(title: "\n\(recipe.title ?? "Le dish")", servings: recipe.servings, readyInMinutes: recipe.readyInMinutes)
         }
-        
-        let processor = DownsamplingImageProcessor(size: foodImage.bounds.size) |> RoundCornerImageProcessor(cornerRadius: foodImage.layer.cornerRadius)
-        foodImage.kf.indicatorType = .activity
-        (foodImage.kf.indicator?.view as? UIActivityIndicatorView)?.color = #colorLiteral(red: 0.8859762549, green: 0.6057382822, blue: 0.4648851752, alpha: 1)
-        foodImage.kf.setImage(with: imageURL,
+        if let recipeDetails = recipeDetails {
+            loadImage(url: recipeDetails.imageURL)
+            foodInfoLabel.configureAttributedText(title: "\n\(recipeDetails.title ?? "Le dish")", servings: recipeDetails.servings, readyInMinutes: recipeDetails.readyInMinutes)
+        }
+    }
+    
+    private func loadImage(url: URL?) {
+        let processor = DownsamplingImageProcessor(size: foodImage.bounds.size)
+        foodImage.kf.setImage(with: url,
                               options: [.processor(processor),
                                         .scaleFactor(UIScreen.main.scale),
                                         .onFailureImage(UIImage(systemName: .photoFill)),

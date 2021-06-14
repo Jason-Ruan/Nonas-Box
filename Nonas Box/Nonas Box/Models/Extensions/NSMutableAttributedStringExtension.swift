a1c4097286e4b8861975f837729814f3b899c064
Jason Ruan <jasonruan@pursuit.org>
2021-06-14 11:14:20 -0400

    Adds extension for NSMutableAttributedString

--- Nonas Box/Nonas Box/Models/Extensions/NSMutableAttributedStringExtension.swift
+++ Nonas Box/Nonas Box/Models/Extensions/NSMutableAttributedStringExtension.swift
@@ -11,7 +11,20 @@ import UIKit
 extension NSMutableAttributedString {
     
     static func superscript(string: String) -> NSMutableAttributedString {
-        return NSMutableAttributedString(string: string, attributes: [.font : UIFont.boldSystemFont(ofSize: 12)])
+        return NSMutableAttributedString(string: string, attributes: [.font: UIFont.boldSystemFont(ofSize: 12)])
     }
     
+    func appendImageAsText(systemName: SystemImageNames) {
+        guard let image = UIImage(systemName: systemName) else { return }
+        let imageTextAttachmentString = NSAttributedString(attachment: NSTextAttachment(image: image))
+        self.append(imageTextAttachmentString)
+    }
+    
+    func appendLineBreaks(_ count: Int?) {
+        self.append(NSAttributedString(string: String(repeating: "\n", count: count ?? 1)))
+    }
+    
+    func appendString(string: String, attributes: [NSAttributedString.Key: Any]? = nil) {
+        self.append(NSAttributedString(string: string, attributes: attributes))
+    }
 }

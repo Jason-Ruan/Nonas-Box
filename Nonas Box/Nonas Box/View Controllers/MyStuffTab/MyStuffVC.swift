fa68ce0c35d4b33d9446428177d3a664a30a65ba
Jason Ruan <jasonruan@pursuit.org>
2021-06-14 11:33:14 -0400

    Fixes formatting in MyStuffVC

--- Nonas Box/Nonas Box/View Controllers/MyStuffTab/MyStuffVC.swift
+++ Nonas Box/Nonas Box/View Controllers/MyStuffTab/MyStuffVC.swift
@@ -15,22 +15,24 @@ public enum MyStuffButtonOptions: String, CaseIterable {
 }
 
 class MyStuffVC: UIViewController {
+    // MARK: - Private TypeAliases
+    private typealias MenuOptionCell = MyStuffOptionCollectionViewCell
     
-    //MARK: - UI Objects
+    // MARK: - UI Objects
     private lazy var myStuffButtonsCollectionView: UICollectionView = {
         let cv = UICollectionView(scrollDirection: .vertical,
                                   spacing: 5,
                                   scrollIndicatorsIsVisible: true,
                                   shouldInset: true)
-        cv.register(MyStuffOptionCollectionViewCell.self, forCellWithReuseIdentifier: MyStuffOptionCollectionViewCell.identifier)
+        cv.register(MenuOptionCell.self, forCellWithReuseIdentifier: MenuOptionCell.identifier)
         cv.dataSource = self
         cv.delegate = self
         return cv
     }()
     
-    //MARK: - Private Properties
+    // MARK: - Private Properties
     
-    //MARK: - LifeCycle Methods
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         setBackgroundColor()
         setUpViews()
@@ -39,7 +41,6 @@ class MyStuffVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = true
     }
-    
     
     // MARK: - Private Functions
     private func setBackgroundColor() {
@@ -58,15 +59,14 @@ class MyStuffVC: UIViewController {
     
 }
 
-
 extension MyStuffVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    //MARK: - CollectionView DataSource and Delegate Methods
+    // MARK: - CollectionView DataSource and Delegate Methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return MyStuffButtonOptions.allCases.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyStuffOptionCollectionViewCell.identifier, for: indexPath) as? MyStuffOptionCollectionViewCell else { return UICollectionViewCell() }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MenuOptionCell.identifier, for: indexPath) as? MenuOptionCell else { return UICollectionViewCell() }
         cell.myStuffButtonOption = MyStuffButtonOptions.allCases[indexPath.row]
         return cell
     }
@@ -76,17 +76,15 @@ extension MyStuffVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLay
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? MyStuffOptionCollectionViewCell else { return }
+        guard let cell = collectionView.cellForItem(at: indexPath) as? MenuOptionCell else { return }
         switch cell.myStuffButtonOption {
-            case .checkInventory:
-                navigationController?.pushViewController(InventoryVC(), animated: true)
-            case .addToInventory:
-                navigationController?.pushViewController(BarcodeScanVC(), animated: true)
-            default:
-                print("Not a valid option")
+        case .checkInventory:
+            navigationController?.pushViewController(InventoryVC(), animated: true)
+        case .addToInventory:
+            navigationController?.pushViewController(BarcodeScanVC(), animated: true)
+        default:
+            print("Not a valid option")
         }
         
     }
-    
-    
 }
